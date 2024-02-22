1e4991c4300dda86210a3a47eb772a65a733adc0
Roberto Leitao <humanberto@gmail.com>
2024-02-21 18:51:00 -0800

    add script to file

---
+++ script.js
@@ -0,0 +1,127 @@
+// // STEP 1
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+
+// // STEP 2
+
+// let age = 42;
+// let name = "Roberto";
+// let student = true;
+// let x = null;
+
+// // STEP 3
+// let purchasePrice = 100000;
+// let commission = 0.06;
+// let totalCommission = purchasePrice * commission;
+// let sellingPrice = purchasePrice + totalCommission;
+
+// // STEP 4
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let intZipCode;
+// let intYourAge;
+// let strReferralSource;
+// let boolMayWeContactYou;
+
+// // STEP 5
+// strFirstName = "Roberto";
+// strLastName = "Leitao";
+// strAddress = "4888 Idaho St";
+
+// STEP 6
+// const age = 42;
+// const name = "Roberto";
+// const studentInfo = name + ", " + age;
+// console.log(studentInfo);
+
+// STEP 7
+// First variable: Boolean and string
+// let boolStudentCurrent = true;
+// let strStudentName = "Roberto";
+// console.log("Is " + strStudentName + " a current student? " + boolStudentCurrent);
+
+// // Second variable: Number and Boolean
+// let studentFee = 100;
+// let boolStudentPaid = true;
+// console.log("Did " + strStudentName + " pay his fee? " + (boolStudentPaid ? "Yes" : "No"));
+
+// STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// It is not valid because the ' in "I'm" is being interpreted as a single quote. To fix this, I used a backslash before ' in order for it to be interpreted as an apostrophe instead of a single quote.
+
+// // STEP 9
+// let nullVariable = null;
+// console.log(nullVariable);
+
+// let undefinedVariable;
+// console.log(undefinedVariable);
+
+
+// STEP 10
+// let age = 42;
+// let firstName = "Roberto";
+// let student = true;
+// let undefinedVariable;
+// let newStudent = {
+//   name: firstName,
+//   age: age,
+//   city: "San Diego"
+// };
+
+// console.log(typeof firstName);
+// console.log(typeof age);
+// console.log(typeof student);
+// console.log(typeof newStudent);
+// console.log(typeof undefinedVariable);
+
+// STEP 11, 12, 12, 14
+
+// let name = "Roberto Leitao";
+// let course = "JaveScript";
+// let welcomeMessage = "Hello " + name + ".\nWelcome to the " + course + " class!";
+// alert(welcomeMessage);
+
+// STEP 15, 16
+// let name = promp("What is your name?");
+// let course = prompt("What class are you taking?");
+// let welcomeMessage = "Hello " + name + ".\nWelcome to the " + course + " class!";
+// console.log(welcomeMessage);
+
+// STEP 17
+let x = 10;
+let y = 20;
+console.log(x + y);
+
+// STEP 18
+let x = 20;
+x += 20;
+console.log(x);
+
+// STEP 19
+let x = 20;
+x *= 5;
+console.log(x);
+
+// STEP 20
+let x = 20 % 3;
+x /= 1;
+console.log(x);
+
+// STEP 21
+let a = 5;
+let b = 10;
+let c = 15;
+console.log((a < b) && (b < c)); 
+
+// STEP 22
+let d = 20;
+let e = 25;
+console.log((d > e) || (d == e));
